a554dfc188a5d5ad27289f8ab16cd5e5b3d84cf4
pradnya <pradnyayadav2642004@gmail.com>
2025-08-12 14:07:01 +0530

    Update model download code to use Google Drive link

--- download_model.py
+++ download_model.py
@@ -1,11 +1,13 @@
 import gdown
 import os
 
-# Replace with your own Google Drive file ID
-url = "https://drive.google.com/drive/folders/1Ldcq-ub_CEds3QADCHRHAj2p3ASm8J34"
-output = os.path.join("model", "fire_model.h5")
+# Extracted file ID from your link
+file_id = "1BBQt3ERqucWUjhV8fjij-QWmRXR9eKVY"
+url = f"https://drive.google.com/uc?id={file_id}"
 
+output = os.path.join("model", "fire_model.h5")
 os.makedirs("model", exist_ok=True)
+
 print("📥 Downloading model from Google Drive...")
 gdown.download(url, output, quiet=False)
 print("✅ Model downloaded to", output)
